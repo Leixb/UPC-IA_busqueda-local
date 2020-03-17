802432e3aa558f3bc45109b88a7cb6a0e547c3bd
Leixb <abone9999@gmail.com>
2020-03-18 00:13:01 +0100

    Add stubs

---
+++ src/main/java/busqueda/DFS/DFSHeuristicFunction.java
@@ -0,0 +1,19 @@
+package busqueda.DFS;
+
+import aima.search.framework.HeuristicFunction;
+
+public class DFSHeuristicFunction implements HeuristicFunction {
+
+    @Override
+    public boolean equals(Object arg0) {
+        // TODO Auto-generated method stub
+        return super.equals(arg0);
+    }
+
+    @Override
+    public double getHeuristicValue(Object arg0) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
---
+++ src/main/java/busqueda/DFS/DFSSuccessorFunction.java
@@ -0,0 +1,16 @@
+package busqueda.DFS;
+
+import aima.search.framework.Successor;
+import aima.search.framework.SuccessorFunction;
+
+import java.util.List;
+
+public class DFSSuccessorFunction implements SuccessorFunction {
+
+    @Override
+    public List getSuccessors(Object arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
