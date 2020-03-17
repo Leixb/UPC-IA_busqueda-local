1d59982d412b6cde68423f5d65c06f781c4f2c31
Leixb <abone9999@gmail.com>
2020-03-18 00:13:01 +0100

    Add stubs

---
+++ src/main/java/busqueda/DFS/DFSSuccessorFunctionSA.java
@@ -0,0 +1,16 @@
+package busqueda.DFS;
+
+import aima.search.framework.Successor;
+import aima.search.framework.SuccessorFunction;
+
+import java.util.List;
+
+public class DFSSuccessorFunctionSA implements SuccessorFunction {
+
+    @Override
+    public List getSuccessors(Object arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
