98fb1b5bc111091302bc90151cbf7596cbca3a93
Leixb <abone9999@gmail.com>
2020-03-18 16:50:15 +0100

    Implementado heuristicas
    close #11

--- src/main/java/busqueda/dfs/DFSEstado.java
+++ src/main/java/busqueda/dfs/DFSEstado.java
@@ -1,7 +1,9 @@
 package busqueda.dfs;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import IA.DistFS.Requests;
@@ -39,7 +41,44 @@ public class DFSEstado {
         this.servidor = Arrays.copyOf(estado, estado.length);
     }
 
-    // changes server that gives file for ith request.
+    // Maximo transmission total de los servidores
+    public int getHeuristicValueMax() {
+        //return Collections.max(transmissionTimes());
+        return transmissionTimes().stream().reduce(0, Integer::max);
+    }
+
+    private List<Integer> transmissionTimes() {
+        List<Integer> transTime = new ArrayList<Integer>(servers.size());
+        for (int i=0; i < requests.size(); ++i) {
+            // [UserID, FileID]
+            final int userID = requests.getRequest(i)[0];
+            final int serverID = servidor[i];
+            final int time = transTime.get(i) + servers.tranmissionTime(serverID, userID);
+            transTime.set(serverID, time);
+        }
+        return transTime;
+    }
+
+
+    // Suma total de transmission total de los servidores con penalizacion por
+    // cargas muy distintas
+    public double getHeuristicValueTotal() {
+        List<Integer> transTimes = transmissionTimes();
+
+        final int totalTime = transTimes.stream().reduce(0, Integer::sum);
+
+        final double mean = totalTime/servers.size();
+
+        double sd = 0.0; // standard deviation
+        for (Integer time : transTimes) {
+            sd += Math.pow(mean - time, 2);
+        }
+        sd = Math.sqrt(sd/servers.size());
+
+        return totalTime*sd; //TODO: mejorar como hacer esta heuristica
+    }
+
+	// changes server that gives file for ith request.
     // returns false if it fails
     // makes no real sense
     public boolean change(final int i) {
