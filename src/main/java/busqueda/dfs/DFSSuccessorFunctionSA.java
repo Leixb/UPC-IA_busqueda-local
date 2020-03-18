0358e5ea9d007b832308e1f7ee383537dd4c2b5d
LEXHERRE <alexherrero1103@gmail.com>
2020-03-18 17:49:06 +0100

    Implementado SuccessorFunctionSA (Simulated Annealing

--- src/main/java/busqueda/dfs/DFSSuccessorFunctionSA.java
+++ src/main/java/busqueda/dfs/DFSSuccessorFunctionSA.java
@@ -3,14 +3,40 @@ package busqueda.dfs;
 import aima.search.framework.Successor;
 import aima.search.framework.SuccessorFunction;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class DFSSuccessorFunctionSA implements SuccessorFunction {
 
     @Override
-    public List getSuccessors(Object arg0) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    public List<Successor> getSuccessors(Object arg0) {
+        List<Successor>       retVal = new ArrayList<Successor>();
+        DFSEstado             estado = (DFSEstado) arg0;
+        DFSHeuristicFunction  DFSHF  = new DFSHeuristicFunction();
+        Random myRandom = new Random();
+        int i;
+        Integer location = -1;
 
+        i = myRandom.nextInt(estado.size());
+        
+        int item = new Random().nextInt(estado.locations(i).size());
+        int it = 0;
+        for (final Integer loc : estado.locations(i)) {
+            if (it == item) {
+                location = loc;
+                break;
+            }
+            it++;
+        }
+
+        DFSEstado newEstado = new DFSEstado(estado.getEstado());
+        newEstado.set(i, location);
+
+        final double v = DFSHF.getHeuristicValue(newEstado);
+        final String S = String.format("Request: %i Coste(%i) ---> %s", i, v, newEstado.toString());
+        retVal.add(new Successor(S, newEstado));
+
+        return retVal;
+    }
 }
